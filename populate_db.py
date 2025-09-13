f925694c7eeb54ea4b38e3879ca42406df2a258d
Alejandro Gemin Rosales <alegemin07@gmail.com>
2025-09-12 22:16:21 -0300

    Popula banco de dados

---
+++ populate_db.py
@@ -0,0 +1,15 @@
+from db import DB
+import os
+import pandas as pd
+
+db = DB(
+    host='localhost',
+    port=5432,
+    database='postgres',
+    user='postgres',
+    password='postgres')
+
+for file in os.listdir("02-silver-validated"):
+    df = pd.read_parquet(f"02-silver-validated/{file}")
+    db.create_table(file.replace(".parquet", ""), df.columns.tolist())
+    db.insert_data(file.replace(".parquet", ""), df)
